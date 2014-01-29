6572da6e1826b5e895cb4bf97b1bb2362a9ac935
Deni <liquid.yethee@hotmail.com>
2014-01-29 13:38:12 +0400

    Added configs for CI server

---
+++ sami.php
@@ -0,0 +1,18 @@
+<?php
+
+use Sami\Sami;
+use Symfony\Component\Finder\Finder;
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->in(__DIR__ . '/')
+    ->exclude(array('Tests', 'vendor'));
+
+return new Sami($iterator, array(
+    'theme'                => 'enhanced',
+    'title'                => 'BiplaneYandexDirectBundle API',
+    'build_dir'            => __DIR__ . '/build/api',
+    'cache_dir'            => __DIR__ . '/build/sami-cache',
+    'default_opened_level' => 2,
+));
