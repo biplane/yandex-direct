74b7e281dbc0a680315dd82243cdb25b97381300
yethee <liquid.yethee@hotmail.com>
2020-10-11 14:39:49 +0300

    Deprecate constants of API errors

--- src/Exception/ApiException.php
+++ src/Exception/ApiException.php
@@ -9,58 +9,269 @@ namespace Biplane\YandexDirect\Exception;
  */
 class ApiException extends RequestException
 {
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const INVALID_CAMPAIGN_ID = 1;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const CAMPAIGN_STATS_NOT_FOUND = 2;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const INVALID_BANNER_ID = 23;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const INVALID_BANNER = 27;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const INVALID_PHRASE_ID = 28;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const INVALID_CAMPAIGN_TYPE = 29;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const AUTHENTICATION_TEMPORARILY_UNAVAILABLE = 52;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const AUTHORIZATION_ERROR = 53;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const INSUFFICIENT_PRIVILEGES = 54;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const EXCEEDED_LIMIT_REQUESTS = 56;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const INCOMPLETE_REGISTRATION = 58;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const INCORRECT_PARAMETERS = 71;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const FORECAST_ID_INVALID = 72;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const FORECAST_NOT_FOUND = 73;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const FORECAST_PENDING = 74;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const FORECAST_PHRASE_INVALID = 76;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const FORECAST_GEO_INVALID = 77;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const FORECAST_NOT_AVAILABLE = 78;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const WORDSTAT_REPORT_IS_BEING_GENERATED = 92;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const INVALID_CAMPAIGN_INFO = 111;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const CAMPAIGNS_OVERFLOW = 114;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const INVALID_BANNER_INFO = 151;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const DEFICIENCY_POINTS = 152;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const OVERFLOW_BANNERS = 153;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const INVALID_POINTONMAP_STRUCT = 154;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const OPERATION_NOT_ALLOWED = 155;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const FORBIDDEN_CHANGE_ARCHIVED_ITEM = 156;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const CAMPAIGN_OVERFLOW_BANNERS = 157;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const FORBIDDEN_CHANGE_CONTEXT_PRICE = 191;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const INVALID_MINUS_WORDS = 192;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const INVALID_PRICE = 242;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const INVALID_AUTOBROKER_STATE = 243;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const LOGIN_INVALID = 251;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const LOGIN_DUPLICATE = 252;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const LOGIN_CREATING_ERROR = 253;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const CLIENT_NOT_EXIST = 259;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const INVALID_FINANCE_TOKEN = 350;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const INVALID_FINANCE_NUMBER = 351;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const INTERNAL_ERROR = 500;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const BAD_REQUEST = 501;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const TEMPORARILY_UNAVAILABLE = 503;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const EXCEEDED_LIMIT_CONCURRENT_REQUESTS = 506;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const ACCESS_DENIED = 510;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const LOGIN_NOT_CONNECTED_TO_DIRECT = 513;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const SERVER_TEMPORARILY_UNAVAILABLE = 1000;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const INITIALIZATION_ERROR = 1001;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const OPERATION_ERROR = 1002;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const UNSUPPORTED = 3500;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const PARAMETERS_INCORRECT = 4000;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const ELEMENTS_OUT_OF_RANGE = 7000;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const EXCEEDED_MAX_NUMBER_OF_OBJECTS = 7001;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const INCORRECT_REQUEST = 8000;
+
+    /**
+     * @deprecated Will be removed in version 5.0
+     */
     const ENTITY_DUPLICATED = 9800;
 
     private $methodRef;
